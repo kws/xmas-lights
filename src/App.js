b912d9dc4fdc7ca54e81cb45754a793fad356675
Kaj Siebert <kaj@k-si.com>
2022-12-05 10:53:03 +0100

    Add build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { SliderPicker, CirclePicker } from 'react-color';
 import './App.css';
+import {useState, useEffect} from "react";
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { v4 as uuidv4 } from 'uuid';
+import { useCookies } from 'react-cookie';
 
 function App() {
+  const [cookie, setCookie] = useCookies(['sfmood']);
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("#FF0000");
+  const [selectedColor, setSelectedColor] = useState(color);
+  const { sendMessage, lastMessage, readyState } = useWebSocket("wss://mood.sfintra.net");
+
+  useEffect(() => {
+    if (!cookie.uuid) {
+      setCookie('uuid', uuidv4())
+    }
+  }, [cookie, setCookie])
+
+  useEffect(() => {
+    console.log(color);
+    sendMessage(JSON.stringify({uuid: cookie.uuid, rgb: selectedColor.rgb, name}));
+  }, [selectedColor, name, cookie]);
+
+  const setButtonColor = (r, g, b) => {
+    setColor({rgb: {r, g, b, a:1}});
+    setSelectedColor({rgb: {r, g, b, a:1}});
+  }
+
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+
+        {readyState === ReadyState.OPEN &&
+          <>
+            <div>
+              <button style={{ backgroundColor: 'black', width: '50px', height: '25px'}} onClick={() => setButtonColor(0, 0, 0)}></button>
+              <button style={{ backgroundColor: 'red', width: '50px', height: '25px'}} onClick={() => setButtonColor(255, 0 ,0)}></button>
+              <button style={{ backgroundColor: 'green', width: '50px', height: '25px'}} onClick={() => setButtonColor(0, 255, 0)}></button>
+              <button style={{ backgroundColor: 'blue', width: '50px', height: '25px'}} onClick={() => setButtonColor(0, 0, 255)}></button>
+              <button style={{ backgroundColor: '#FF0', width: '50px', height: '25px'}} onClick={() => setButtonColor(255, 255 ,0)}></button>
+              <button style={{ backgroundColor: '#0FF', width: '50px', height: '25px'}} onClick={() => setButtonColor(0, 255, 255)}></button>
+              <button style={{ backgroundColor: '#F0F', width: '50px', height: '25px'}} onClick={() => setButtonColor(255, 0, 255)}></button>
+            </div>
+          </>
+
+
+        }
+        {readyState !== ReadyState.OPEN &&
+
+          <p>{connectionStatus}</p>
+        }
+
+
+    </main>
   );
 }
 
